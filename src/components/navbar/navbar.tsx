6ed627a702d04f6e1c6b68b534df00af698715e6
kard sahaphong <kardsahaphong@kards-MacBook-Air.local>
2022-07-09 16:51:01 +0700

    UI hamburger

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './navbar.less';
 
 function Navbar() {
-    return(
+    const [clickHam, setClickHam] = useState(false);
+    return (
         <div className='container'>
             <span>KARD SAHAPHONG</span>
-            <div className='hamburger'>
-                <div className='ham1'></div>
-                <div className='ham2'></div>
-                <div className='ham3'></div>
+            <div
+                className='hamburger'
+                onClick={() => setClickHam(!clickHam)}
+            >
+                <div
+                    className='ham1'
+                    style={{ transform: clickHam ? 'rotate(45deg) translate(4px, 4px)' : 'none' }}
+                ></div>
+                <div
+                    className={clickHam ? '' : 'ham2-appear'}
+                    style={{ height: clickHam ? '0' : '3px' }}
+                ></div>
+                <div
+                    className='ham3'
+                    style={{ transform: clickHam ? 'rotate(-45deg) translate(4px, -3px)' : 'none' }}
+                ></div>
             </div>
         </div>
     )
